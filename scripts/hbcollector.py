6cdccc58e6ed49a8de9403856f19c776d8f92228
Tomasz Radziewicz <Tomasz.Radziewicz@torad.pl>
2014-04-05 12:32:12 +0200

    Added initial version of heartbeat collector and heartbeat monitor

---
+++ scripts/hbcollector.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+#
+# Data collector for heartbeat monitor
+#
+# Stores last timestamp of message identified by location and event name
+
+import sys
+import socket
+import argparse
+import time
+import json
+import bsddb
+import panmsg
+import streem
+
+#-----------------------------------------------------------------------------
+# Creates commandline parser
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Monitors Panopticons probe messages"
+    )
+    parser.add_argument('--host', '-a', dest='host', type=str,
+        required=True, help='Destination streem host')
+    parser.add_argument('--port', '-p', dest='port', type=int,
+        required=True, help='Destination streem port')
+    parser.add_argument('--src-channel', '-s', dest='srcchannel', type=str,
+        default='probes', help='Source streem channel')
+    parser.add_argument('--dst-channel', '-d', dest='dstchannel', type=str,
+        default='states', help='Destination streem channel')
+    parser.add_argument('--db-name', '-db', dest='dbname', type=str,
+        default='hbdb.db', help='Heartbeat database name')
+    return parser
+
+#-----------------------------------------------------------------------------
+# Gets data identyfying message
+def get_message_key(message):
+    key = {
+        "location": message["location"],
+        "event": {
+            "name": message["event"]["name"]
+        }
+    }
+    return json.dumps(key)
+
+#-----------------------------------------------------------------------------
+# Creates state message
+def create_state_message(message, state):
+    state_message = {
+        "v": 2,
+        "time": round(time.time()),
+        "location": message["location"],
+        "event": {
+            "name": message["event"]["name"],
+            "state": state,
+            "expected": ["up"],
+            "attention": ["down"]
+        }
+    }
+    return state_message
+
+#-----------------------------------------------------------------------------
+# Parse commandline arguments
+parser = create_parser()
+args = parser.parse_args()
+
+try:
+    # Open/create database
+    db = bsddb.hashopen(args.dbname, "c")
+
+    # Streem initialization
+    conn = streem.Streem(args.host, args.port)
+    conn.register(args.dstchannel)
+    conn.subscribe(args.srcchannel)
+
+    # Main loop
+    while True:
+        reply = conn.receive()
+        if 'message' in reply:
+            # TODO: Add schema validation
+            message = reply["message"]
+            if message["v"] != 2:
+                continue
+
+            key = get_message_key(message)
+            if not db.has_key(key):
+                state_message = create_state_message(message, "up")
+                conn.submit(state_message)
+
+            db[key] = str(message["time"])
+            db.sync()
+
+except streem.sjcp.ProtocolError as e:
+    print "Streem returned status %s." % e.args
+except KeyboardInterrupt:
+    pass
+finally:
+    db.close()
---
+++ scripts/hbmonitor.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+#
+# Heartbeat monitor
+#
+# Verifies timestamps of messages and notifies, if timestamp of given message
+# was not refreshed for too long time
+
+import sys
+import socket
+import time
+import argparse
+import json
+import bsddb
+import panmsg
+import streem
+
+#-----------------------------------------------------------------------------
+# Creates commandline parser
+def create_parser():
+    parser = argparse.ArgumentParser(description="Monitors Panopticons probe messages")
+    parser.add_argument('--host', '-a', dest='host', type=str,
+        required=True, help='Destination streem host')
+    parser.add_argument('--port', '-p', dest='port', type=int,
+        required=True, help='Destination streem port')
+    parser.add_argument('--channel', '-c', dest='channel', type=str, 
+        default='states', help='Destination streem channel')
+    parser.add_argument('--db-name', '-db', dest='dbname', type=str, 
+        default='hbdb.db', help='Heartbeat database name')
+    parser.add_argument('--interval', '-i', dest='interval', type=int, 
+        default=10, help='Time (in seconds) interval between hearbeats')
+    parser.add_argument('--timeout', '-t', dest='timeout', type=int, 
+        default=60, help='Max allowed time (in seconds) interval between probe messages')
+    return parser
+
+#-----------------------------------------------------------------------------
+# Creates message with probes sate
+def create_state_message(message, state):
+    state_message = {
+        "v": 2,
+        "time": round(time.time()),
+        "location": message["location"],
+        "event": {
+            "name": message["event"]["name"],
+            "state": state,
+            "expected": ["up"],
+            "attention": ["down"]
+        }
+    }
+    return message
+
+#-----------------------------------------------------------------------------
+# Checks if given timestamp has timed out
+def timeout_exceeded(value, timeout):
+    timestamp = time.time()
+    if timestamp - value > timeout:
+        return True
+    else:
+        return False
+
+#-----------------------------------------------------------------------------
+
+# Parse arguments
+parser = create_parser()
+args = parser.parse_args()
+
+try:
+    # Open database
+    db = bsddb.hashopen(args.dbname, 'w')
+
+    # Streem initialization
+    conn = streem.Streem(args.host, args.port)
+    conn.register(args.channel)
+    
+    while True:
+        for key in db.keys():
+            if timeout_exceeded(int(db[key]), args.timeout):
+                message = create_state_message(json.loads(key), "down")
+                conn.submit(message)
+                del db[key]
+                db.sync()
+        time.sleep(args.interval)
+
+except streem.sjcp.ProtocolError as e:
+    print "Streem returned status %s." % e.args
+except KeyboardInterrupt:
+    pass
+finally:
+    db.close()
