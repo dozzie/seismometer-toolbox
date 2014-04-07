43b9c33633a59ac9ede056612aca7b898c4f6ece
Stanislaw Klekot <dozzie@jarowit.net>
2014-04-07 23:30:37 +0200

    Killed hack-state-forwarder in favour of plugin to pull-push-bridge.
    hack-state-forwarder did the same work as the bridge anyway.

--- lib/panopticon/bridge/state_forwarder.py
+++ lib/panopticon/bridge/state_forwarder.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python
-
+#
 # instead of having separate hack-state-forwarder.py I could just use this
 # plugin to pull-push-bridge
+#
+
+import streem
 
 class PullPushBridge:
   def __init__(self, options):
-    self.collectd_socket = options.destination
-    #self.socket = 
+    (self.host, self.port, self.channel) = options.destination.split(":", 2)
+    self.port = int(self.port)
+    self.conn = streem.Streem(self.host, self.port)
+    self.conn.register(self.channel)
 
   def send(self, message):
-    pass
+    try:
+      # FIXME: hardcoded for ModMon::Event v=1
+      if (message.get('v') == 1) and 'value' in message['event']['state']:
+        self.conn.submit(message)
+    except KeyError:
+      pass # it's OK to not find a key in hash
 
 #-----------------------------------------------------------------------------
 # vim:ft=python:foldmethod=marker
