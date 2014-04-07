e2ab7f46ee1c04cb71b1b265a4daa2847bb65284
Stanislaw Klekot <dozzie@jarowit.net>
2014-04-08 00:58:01 +0200

    collectd plugin for pull-push-bridge written.
    It has quite inflexible name conversion, but it shouldn't be a problem.

--- lib/panopticon/pull_push_bridge/collectd.py
+++ lib/panopticon/pull_push_bridge/collectd.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python
 
-# PUTVAL $host/$plugin/$type-$instance $time:$value
-# $value, $value_name -- from vset
-# $host = $m{location}{host}
-# $plugin = "streem"
-# $type = "gauge"
-# $instance = sprintf "%s+%s+%s",
-#                     $m{location}{service}, $m{event}{name}, $value_name
+import socket
+import re
 
 class PullPushBridge:
+  NON_WORD = re.compile(r'[^a-zA-Z0-9_.]')
+
   def __init__(self, options):
     self.collectd_socket = options.destination
-    #self.socket = 
+    self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    self.socket.connect(self.collectd_socket)
+
+  @staticmethod
+  def encode_location(location, aspect_name, value_name):
+    # FIXME: this naming scheme is very inflexible
+    return "%s+%s+%s" % (
+      PullPushBridge.NON_WORD.sub("_", location['service']),
+      PullPushBridge.NON_WORD.sub("_", aspect_name),
+      PullPushBridge.NON_WORD.sub("_", value_name),
+    )
 
   def send(self, message):
-    pass
+    try:
+      # FIXME: hardcoded for ModMon::Event v=1
+      if (message.get('v') == 1) and 'vset' in message['event']:
+        host = message['location']['host']
+        time = message['time']
+        for n in sorted(message['event']['vset']['value']):
+          name = PullPushBridge.encode_location(
+            message['location'], message['event']['name'], n
+          )
+          if message['event']['vset']['value'][n] is not None:
+            value = message['event']['vset']['value'][n]
+          else:
+            value = 'U'
+          self.socket.send(
+            "PUTVAL %s/panopticon/gauge-%s %d:%s\n" % (host, name, time, value)
+          )
+          self.socket.recv(256) # discard (FIXME: detect submission error)
+    except KeyError:
+      pass # don't die, do nothing
 
 #-----------------------------------------------------------------------------
 # vim:ft=python:foldmethod=marker
