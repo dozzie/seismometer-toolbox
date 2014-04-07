7da74801d5ca1e7ec368f6bec4022e6180aebace
Stanislaw Klekot <dozzie@jarowit.net>
2014-04-08 00:04:29 +0200

    Bridge stdout plugin: changed way of printing message.
    Now it's JSON instead of Python structure.

--- lib/panopticon/pull_push_bridge/stdout.py
+++ lib/panopticon/pull_push_bridge/stdout.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
+import json
+
 class PullPushBridge:
   def __init__(self, options):
     pass
 
   def send(self, message):
-    print message
+    print json.dumps(message, sort_keys = True)
 
 #-----------------------------------------------------------------------------
 # vim:ft=python:foldmethod=marker
