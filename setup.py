8eba6209c5c7ada592a999346efc377b6838c040
Stanislaw Klekot <dozzie@jarowit.net>
2014-05-23 18:27:27 +0200

    Added installing bin/*.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages
+from glob import glob
 
 setup(
   name         = 'panopticon-toolbox',
   version      = '0.0.1',
   description  = "Small tools for Panopticon",
+  scripts      = glob("bin/*"),
   packages     = find_packages("lib"),
   package_dir  = { "": "lib" },
 )
