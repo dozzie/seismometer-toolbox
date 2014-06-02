5058686330507f5c4b054dab1a1dcae43121ce15
Stanislaw Klekot <dozzie@jarowit.net>
2014-06-02 23:24:50 +0200

    Added memory spooler (initial version).

--- lib/panopticon/messenger/spool.py
+++ lib/panopticon/messenger/spool.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python
 
+import collections
+
 #-----------------------------------------------------------------------------
 
 #   * message spooler
 #     * works on strings
 #     * in-memory
 #     * on-disk
+
+#-----------------------------------------------------------------------------
+
+class MemorySpooler:
+  def __init__(self, max = None):
+    self._queue = collections.deque()
+    self._max = max
+    self._size = 0
+
+  def spool(self, line):
+    self._size += len(line)
+    self._queue.append(line)
+    if self._max is not None:
+      while self._size > self._max:
+        self.drop_one()
+
+  def peek(self):
+    if self._size == 0:
+      return None
+    else:
+      return self._queue[0]
+
+  def drop_one(self):
+    line = self._queue.popleft()
+    self._size -= len(line)
 
 #-----------------------------------------------------------------------------
 # vim:ft=python:foldmethod=marker
