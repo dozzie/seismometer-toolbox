b14c7cb5779e34e00f1c533379b38c1d8ecfbb7b
Stanislaw Klekot <dozzie@jarowit.net>
2015-07-12 19:30:35 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 from glob import glob
 
 setup(
-  name         = 'seismometer-toolbox',
-  version      = '0.0.2',
-  description  = "Small tools for Seismometer",
-  scripts      = glob("bin/*"),
-  packages     = find_packages("lib"),
-  package_dir  = { "": "lib" },
+    name         = 'seismometer-toolbox',
+    version      = '0.0.0', # unspecified at this branch
+    description  = "Small tools for Seismometer",
+    scripts      = glob("bin/*"),
+    packages     = find_packages("lib"),
+    package_dir  = { "": "lib" },
 )
